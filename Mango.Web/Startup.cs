24a77c2e4ce412dd4ae0613606e4e1881ab096f1
Fady Soliman <fadysoliman160@hotmail.com>
2021-09-17 01:50:40 +0200

    UI tweaks + dependency injection to products service and http client

--- Mango.Web/Startup.cs
+++ Mango.Web/Startup.cs
@@ -1,3 +1,5 @@
+using Mango.Web.Services;
+using Mango.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +26,9 @@ namespace Mango.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            ApplicationSettings.ProductApiBase = Configuration["ServiceUrls:ProductApi"];
+            services.AddHttpClient<IProductService, ProductService>();
+            services.AddScoped<IProductService, ProductService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
