6054b325c6c3f6c148c4a4b3797532ed3cb86fee
Fady Soliman <fadysoliman160@hotmail.com>
2021-10-07 22:12:00 +0200

    Coupon controller

---
+++ Mango.Services.CouponApi/Controllers/CouponController.cs
@@ -0,0 +1,42 @@
+﻿using Mango.Services.CouponApi.DTO;
+using Mango.Services.CouponApi.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Mango.Services.CouponApi.Controllers
+{
+    [Route("api/coupons")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private readonly ICouponRepository _couponRepository;
+
+        public CouponController(ICouponRepository couponRepository)
+        {
+            _couponRepository = couponRepository;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Route("{code}")]
+        public async Task<ResponseDto<CouponDto>> Get(string code)
+        {
+            ResponseDto<CouponDto> response = new();
+
+            try
+            {
+                response.Result = await _couponRepository.Get(code);
+                response.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages.Add(ex.Message);
+            }
+
+            return response;
+        }
+    }
+}
