fb4912ab22f1a128e5d76d23dce7768893c24d1d
Fady Soliman <fadysoliman160@hotmail.com>
2021-10-14 19:52:57 +0200

    creating order api project

---
+++ Mango.Services.OrderApi/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.OrderApi.DbContexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Mango.Services.OrderApi/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace Mango.Services.OrderApi
+{ 
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            return new MapperConfiguration(config =>
+            {
+                //config.CreateMap<ProductDto, Product>().ReverseMap();
+                
+            });
+        }
+    }
+}
